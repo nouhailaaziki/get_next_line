6be35a91820e02ecb14c9761cd93e59ddb60f93a
nouhaila <cdnouhauniverse@gmail.com>
2024-12-03 12:48:29 +0100

    Bonus done

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: noaziki <noaziki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/30 15:58:55 by noaziki           #+#    #+#             */
-/*   Updated: 2024/12/02 14:44:08 by noaziki          ###   ########.fr       */
+/*   Created: 2024/12/03 12:16:07 by noaziki           #+#    #+#             */
+/*   Updated: 2024/12/03 12:28:46 by noaziki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*find_line(int fd, char *rest, char *buffer)
 {
@@ -59,28 +59,29 @@ char	*fix_line(char *bufl)
 
 char	*get_next_line(int fd)
 {
-	static char	*rest;
+	static char	*rest[OPEN_MAX];
 	char		*line;
 	char		*buffer;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX
-		|| fd >= OPEN_MAX)
-		return (free (rest), rest = NULL, NULL);
+	if (fd < 0 || fd >= OPEN_MAX)
+		return (NULL);
+	if (BUFFER_SIZE <= 0 || BUFFER_SIZE >= INT_MAX)
+		return (free(rest[fd]), rest[fd] = NULL);
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
-		return (free (rest), rest = NULL, NULL);
+		return (free (rest[fd]), rest[fd] = NULL, NULL);
 	buffer[0] = 0;
-	line = find_line(fd, rest, buffer);
+	line = find_line(fd, rest[fd], buffer);
 	if (!line)
-		return (rest = NULL, NULL);
-	rest = fix_line(line);
-	if (!rest)
+		return (rest[fd] = NULL, NULL);
+	rest[fd] = fix_line(line);
+	if (!rest[fd])
 		return (free(line), NULL);
 	if (line[0] == 0)
-		return (free (rest), free (line), rest = NULL);
+		return (free (rest[fd]), free (line), rest[fd] = NULL);
 	buffer = ft_strdup(line);
 	free(line);
 	if (!buffer)
-		return (free(rest), rest = NULL, NULL);
+		return (free(rest[fd]), rest[fd] = NULL, NULL);
 	return (buffer);
 }
